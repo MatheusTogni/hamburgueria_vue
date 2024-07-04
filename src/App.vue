1d1f5caa3de03f24d4a23f609be87dedff785911
DuqueQz <matheustopgni64@gmail.com>
2024-07-04 17:12:17 -0300

    aula 22

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <Navbar />
-  <router-view/>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
   <Footer />
 </template>
 
@@ -12,10 +12,33 @@ export default {
   components: {
     Navbar,
     Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "Faça seu Hambúrguer"
+    }
   }
 }
 </script>
 
 <style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
 
+.main-container {
+  margin: 50px;
+  min-height: 250px;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: #222;
+}
 </style>
